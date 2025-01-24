899046ba9a548a2e1fe85f9f6540635dd5474dd3
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2025-01-24 11:21:28 +0500

    Add files via upload

---
+++ app/newprices/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header_1 from '../header_1'
+import Header0 from '../header0'
+import Header1 from '../header1'
+import SanityData from "../sanitydata";
+//import Image from 'next/image'
+//import whitesmall from "./whitesmall.jpg"
+//import Navbarnew from '../navbarnew'
+import Aboutcomponent1 from '../aboutcomponent1'
+import Aboutcomponent2 from '../aboutcomponent2'
+import Aboutcomponent3 from '../aboutcomponent3'
+//import Link from 'next/link'
+import Footerhk2 from "../Footer2hk";
+
+const Newprices = () => {
+  return (
+   <>
+   		<Header_1/>
+	<Header0/>
+	<Header1/>
+ <SanityData/>
+	 <Footerhk2/>
+   </>
+      
+     
+   
+  );
+}
+export default Newprices;
