ee35fc113ecb16216e85b44b7a9a1c186606e729
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2025-01-24 11:20:47 +0500

    Add files via upload

---
+++ app/About/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header_1 from '../header_1'
+import Header0 from '../header0'
+import Header1 from '../header1'
+//import Image from 'next/image'
+//import whitesmall from "./whitesmall.jpg"
+//import Navbarnew from '../navbarnew'
+import Aboutcomponent1 from '../aboutcomponent1'
+import Aboutcomponent2 from '../aboutcomponent2'
+import Aboutcomponent3 from '../aboutcomponent3'
+//import Link from 'next/link'
+import Footerhk2 from "../Footer2hk";
+
+const About = () => {
+  return (
+   <>
+   		<Header_1/>
+	<Header0/>
+	<Header1/>
+<Aboutcomponent1/>
+<Aboutcomponent2/>
+<Aboutcomponent3/>
+	 <Footerhk2/>
+   </>
+      
+     
+   
+  );
+}
+export default About;
