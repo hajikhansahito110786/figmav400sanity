2414c27bf347844f96a4033e3948d6e68e104aa5
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2025-01-24 11:19:49 +0500

    Add files via upload

---
+++ app/SinglePage/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header_1 from '../header_1'
+import Header0 from '../header0'
+import Header1 from '../header1'
+//import Image from 'next/image'
+//import whitesmall from "./whitesmall.jpg"
+//import Navbarnew from '../navbarnew'
+
+import SinglePagecomponent1 from '../singlePagecomponent1'
+
+//import Link from 'next/link'
+import Footerhk2 from "../Footer2hk";
+
+const SinglePage = () => {
+  return (
+   <>
+   		<Header_1/>
+	<Header0/>
+	<Header1/>
+<SinglePagecomponent1/>
+
+	 <Footerhk2/>
+   </>
+      
+     
+   
+  );
+}
+export default SinglePage;
